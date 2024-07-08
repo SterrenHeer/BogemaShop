6fba1370ead7644f25466578ae809d2c4f87ce81
SterrenHeer <sterrenheer@gmail.com>
2024-07-08 20:54:38 +0300

    add media for header with burger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $.get('header.html',function(response){ 
-    $('.header').html(response);
+    $('.header').html(response); 
+    $('.burger_button, .burger_close, .burger a').click(() => {
+        $('.burger').toggleClass('show');
+        $('.burger_dropdown :checked').prop('checked', false);
+    });
+    $(window).on( "resize", () => {
+        $('.burger').removeClass("show")
+        $('.burger_dropdown :checked').prop('checked', false);
+    });
 });
