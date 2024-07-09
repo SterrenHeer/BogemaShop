1bd3e0fab3d3af218f96fa19ce6a8357fcce7ea4
SterrenHeer <sterrenheer@gmail.com>
2024-07-09 14:08:10 +0300

    add bestsellers section with slider

--- js/script.js
+++ js/script.js
@@ -12,3 +12,33 @@ $.get('header.html',function(response){
 $.get('footer.html',function(response){ 
     $('.footer').html(response); 
 });
+
+const changeHeartColor = (hearts) => {
+    hearts.forEach(favorite => {
+        favorite.addEventListener('click', (e) => {
+            let img = favorite.querySelector('img');
+            img.getAttribute('src') == 'icons/heart.svg' ? img.setAttribute('src', 'icons/heart_fill.svg') : img.setAttribute('src', 'icons/heart.svg')
+        });
+    });
+}
+
+if (document.querySelector('.bestsellers_field') != null) {
+    slider({
+        containerSelector: '.bestsellers_container',
+        slideSelector: '.bestsellers_slide',
+        prevSlideSelector: '.bestsellers_prev',
+        nextSlideSelector: '.bestsellers_next',
+        wrapperSelector: '.bestsellers_wrapper',
+        fieldSelector: '.bestsellers_field',
+        elementsPerPage: 4,
+        elementsPerPageMobile: 2,
+        indicatorsClass: `bestsellers_indicators`,
+        columnGap: 30,
+        swipe: true,
+    });
+}
+
+if (document.querySelector('.bestsellers_slide') != null) {
+    bestsellers_hearts = document.querySelectorAll('.bestsellers_favorite');
+    changeHeartColor(bestsellers_hearts);
+}
