79b05b80452ce2c8cfd925c13880f0a3968824f5
SterrenHeer <sterrenheer@gmail.com>
2024-07-11 22:04:07 +0300

    add media for catalog section and fix some elements

--- js/script.js
+++ js/script.js
@@ -13,6 +13,15 @@ $.get('footer.html',function(response){
     $('.footer').html(response); 
 });
 
+if (document.querySelector('.catalog') != null) {
+    $('.filter_button, .filter_close, .filter a').click(() => {
+        $('.filter').toggleClass('show');
+    });
+    $(window).on( "resize", () => {
+        $('.filter').removeClass("show")
+    });
+}
+
 const changeHeartColor = (hearts) => {
     hearts.forEach(favorite => {
         favorite.addEventListener('click', (e) => {
@@ -62,33 +71,36 @@ if (document.querySelector('.catalog_item') != null) {
     changeHeartColor(catalog_hearts);
 }
 
-let slider = document.getElementById('slider');
-
-noUiSlider.create(slider, {
-    start: [0.5, 1000],
-    connect: true,
-    range: {
-        'min': 0.50,
-        'max': 1000
-    }
-});
-let buttons = document.querySelectorAll('.noUi-touch-area')
-buttons.forEach((btn, index) => {
-    let p = document.createElement('div');
-    p.classList.add('catalog_range_value');
-    if (index == 1) {
-        p.classList.add('last')
-    }
-    p.textContent = parseFloat(slider.noUiSlider.get()[index]).toFixed(2);
-    btn.appendChild(p);
-    
-    btn.addEventListener('click', () => {
-        btn.querySelector('.catalog_range_value').innerHTML = parseFloat(slider.noUiSlider.get()[index]).toFixed(2);
+if (document.querySelector('.range_slider') != null) {
+    let ranges = document.querySelectorAll('.range_slider');
+    ranges.forEach((range, index) => {
+        noUiSlider.create(range, {
+            start: [0.5, 1000],
+            connect: true,
+            range: {
+                'min': 0.50,
+                'max': 1000
+            }
+        });
+        let buttons = range.querySelectorAll('.noUi-touch-area')
+        buttons.forEach((btn, index) => {
+            let p = document.createElement('div');
+            p.classList.add('catalog_range_value');
+            if (index == 1) {
+                p.classList.add('last')
+            }
+            p.textContent = parseFloat(range.noUiSlider.get()[index]).toFixed(2);
+            btn.appendChild(p);
+            
+            btn.addEventListener('click', () => {
+                btn.querySelector('.catalog_range_value').innerHTML = parseFloat(range.noUiSlider.get()[index]).toFixed(2);
+            });
+            btn.addEventListener('mousemove', () => {
+                btn.querySelector('.catalog_range_value').innerHTML = parseFloat(range.noUiSlider.get()[index]).toFixed(2);
+            });
+            btn.addEventListener('touchmove', () => {
+                btn.querySelector('.catalog_range_value').innerHTML = parseFloat(range.noUiSlider.get()[index]).toFixed(2);
+            });
+        });
     });
-    btn.addEventListener('mousemove', () => {
-        btn.querySelector('.catalog_range_value').innerHTML = parseFloat(slider.noUiSlider.get()[index]).toFixed(2);
-    });
-    btn.addEventListener('touchmove', () => {
-        btn.querySelector('.catalog_range_value').innerHTML = parseFloat(slider.noUiSlider.get()[index]).toFixed(2);
-    });
-});
+}
